252dbb790817077d4bade018c3de89f2e9976f2d
Sabbir Hossain Rizvi <rizvibd1100@gmail.com>
2025-03-06 22:48:08 +0600

    new file:   src/components/Info.js 	modified:   src/pages/LoginPage.js

---
+++ src/components/Info.js
@@ -0,0 +1,14 @@
+export default function Info() {
+  return (
+    <>
+      <div className="info-container mt-4 text-center">
+        <p className="text-sm text-gray-600">
+          Developed by{" "}
+          <a href="https://github.com/rizvibr0" className="font-bold text-indigo-500">
+            RizviBR0
+          </a>
+        </p>
+      </div>
+    </>
+  );
+}
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuthStore from "../store/authStore";
+import Info from "../components/Info";
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -79,7 +80,7 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8 relative">
       <div className="max-w-md w-full space-y-8 bg-white p-6 rounded-lg shadow-md">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
@@ -202,6 +203,9 @@ const LoginPage = () => {
             )}
           </div>
         </form>
+      </div>
+      <div className="absolute bottom-6 text-center w-full">
+        <Info />
       </div>
     </div>
   );
